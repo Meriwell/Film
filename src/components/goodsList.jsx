5a4138b4be20cabbe918832e295c8ec60f367e32
Meriwell <zakurenova2019@yandex.ru>
2023-06-05 14:27:24 +0300

    film

---
+++ src/components/goodsList.jsx
@@ -0,0 +1,18 @@
+import {GoodItem} from "./GoodItem";
+
+function GoodsList(props) {
+    const {goods = [], addToBasket = Function.prototype } = props;
+
+    if (!goods.length ) {
+        return <h3>Not found</h3>;
+    }
+    
+    return (
+        <div className="goods">
+            {goods.map((item) =>(
+                <GoodItem key={item.imdbID} {...item} addToBasket={addToBasket}/>
+            ))}
+        </div>
+    );
+}
+export { GoodsList};
